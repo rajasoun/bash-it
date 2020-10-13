493cc3359eacd19d6e00601ee4303edeb6115fff
Raja. Soundaramourty <rajasoun@cisco.com>
2020-10-14 00:34:58 +0530

    End To End Script

--- scripts/assist/secrets_manager.bash
+++ scripts/assist/secrets_manager.bash
@@ -27,13 +27,24 @@ case $action in
       echo "${GREEN}Deleting up aws-vault for Profile : $_AWS_PROFILE${NC}"
       aws-vault --backend=file remove  $_AWS_PROFILE
       ;;
-    get-value)
+    set-value) # For Debugging
       read -r -p "${LIGHT_BLUE} Secrets Store Environment ([prod,non-prod]) : ${NC}" ENV_TYPE
       read -r -p "${LIGHT_BLUE} Deployment Environment ([qa,stage,prod]) : ${NC}" DEPLOYMENT_ENV_TYPE
 
-      CLIENT_ID=$(aws-vault --backend=file exec $_AWS_PROFILE -- secretcli get "$_AWS_SECRET_STORE/$ENV_TYPE" "$_AWS_SECRET_STORE_NAME_PATTERN/$DEPLOYMENT_ENV_TYPE/client-id")
-      CLIENT_SECRET=$(aws-vault --backend=file exec $_AWS_PROFILE -- secretcli get "$_AWS_SECRET_STORE/$ENV_TYPE" "$_AWS_SECRET_STORE_NAME_PATTERN/$DEPLOYMENT_ENV_TYPE/client-secret")
+      #aws-vault --backend=file exec -- secretcli init "$_AWS_SECRET_STORE/$ENV_TYPE"
+      aws-vault --backend=file exec $_AWS_PROFILE -- secretcli set "$_AWS_SECRET_STORE/$ENV_TYPE" "$_AWS_SECRET_STORE_NAME_PATTERN/$DEPLOYMENT_ENV_TYPE/client-id" -s
+      aws-vault --backend=file exec $_AWS_PROFILE -- secretcli set "$_AWS_SECRET_STORE/$ENV_TYPE" "$_AWS_SECRET_STORE_NAME_PATTERN/$DEPLOYMENT_ENV_TYPE/client-secret" -s
       echo "${LIGHT_BLUE} client-id: ${CLIENT_ID}   client-secert: ${CLIENT_SECRET}  ${NC}"
+      ;;
+    get-value)  # For Debugging
+      read -r -p "${LIGHT_BLUE} Secrets Store Environment ([prod,non-prod]) : ${NC}" ENV_TYPE
+      read -r -p "${LIGHT_BLUE} Deployment Environment ([qa,stage,prod]) : ${NC}" DEPLOYMENT_ENV_TYPE
+
+      read -r -p "${LIGHT_BLUE}${UNDERLINE} Get Value For Key ([client-id,client-secret]) : ${NC}" KEY
+      echo "${GREEN}Getting value for Key : $_AWS_SECRET_STORE_NAME_PATTERN/$DEPLOYMENT_ENV_TYPE/client-secret"
+      
+      VALUE=$(aws-vault --backend=file exec $_AWS_PROFILE -- secretcli get "$_AWS_SECRET_STORE/$ENV_TYPE" "$_AWS_SECRET_STORE_NAME_PATTERN/$DEPLOYMENT_ENV_TYPE/$KEY" | tr -d "")
+      echo "${LIGHT_BLUE} $KEY -> $VALUE ${NC}"
       ;;
     clean)
       rm -fr  bash-it reports
