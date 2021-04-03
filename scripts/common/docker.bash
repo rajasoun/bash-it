730a18fbb6e3fabbb849875e17488cd357093485
Raja Soundaramourty <rajasoun@icloud.com>
2021-04-03 11:00:19 +0530

    Fix on error

--- scripts/common/docker.bash
+++ scripts/common/docker.bash
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
 function docker_space_before(){
-  CURRENTSPACE=$(docker system df)
   echo "Current Docker Space:" 
+  CURRENTSPACE=$(docker system df)
   echo -e "$CURRENTSPACE"
 }
 
 function docker_find (){
-  REMOVEIMAGES=$(docker images | grep " [days|months|weeks]* ago" | awk '{print $3}')
-
   echo "Listing images that needs to be cleaned up:" 
+  REMOVEIMAGES=$(docker images | grep " [days|months|weeks]* ago" | awk '{print $3}' || echo "None")
   echo -e "$REMOVEIMAGES"
 }
 
 function docker_clean_images(){
   echo "Cleaning images" 
   docker rmi \
-    $(docker images --filter "dangling=true" -q --no-trunc) \
-    2>/dev/null || echo "No more dangling images to remove."
+    "$(docker images --filter "dangling=true" -q --no-trunc)" \
+    2>/dev/null || echo "No dangling images to remove."
   # Always remove untagged images
   docker rmi \
-    $(docker images | grep "<none>" | awk '{print $3}') \
+    "$(docker images | grep "<none>" | awk '{print $3}')" \
     2>/dev/null || echo "No untagged images to delete."
 }
 
 function docker_space_after(){
-  CURRENTSPACE=$(docker system df)
   echo "Current Docker Space, after clean up:" 
+  CURRENTSPACE=$(docker system df)
   echo -e "$CURRENTSPACE"
 }
 
